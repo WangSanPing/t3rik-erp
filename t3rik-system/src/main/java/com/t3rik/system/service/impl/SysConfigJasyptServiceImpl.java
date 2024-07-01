cd60ae13eb4fd9108e827ddecc6fba52e152cc31
t3rik <t3rik@itcast.cn>
2024-07-01 17:06:45 +0800

    feat: 添加工具

---
+++ t3rik-system/src/main/java/com/t3rik/system/service/impl/SysConfigJasyptServiceImpl.java
@@ -0,0 +1,22 @@
+package com.t3rik.system.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.t3rik.system.domain.SysConfigJasypt;
+import com.t3rik.system.mapper.SysConfigJasyptMapper;
+import com.t3rik.system.service.ISysConfigJasyptService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * 加密配置Service业务层处理
+ *
+ * @author t3rik
+ * @date 2024-07-01
+ */
+@Service
+public class SysConfigJasyptServiceImpl extends ServiceImpl<SysConfigJasyptMapper, SysConfigJasypt> implements ISysConfigJasyptService {
+    @Resource
+    private SysConfigJasyptMapper sysConfigJasyptMapper;
+
+}
