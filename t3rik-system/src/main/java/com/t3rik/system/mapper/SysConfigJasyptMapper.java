f0bbf2e55c20165721104e3b54cfa27f0e715185
t3rik <t3rik@itcast.cn>
2024-07-01 17:06:45 +0800

    feat: 添加工具

---
+++ t3rik-system/src/main/java/com/t3rik/system/mapper/SysConfigJasyptMapper.java
@@ -0,0 +1,18 @@
+package com.t3rik.system.mapper;
+
+import java.util.List;
+
+import com.t3rik.system.domain.SysConfigJasypt;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 加密配置Mapper接口
+ *
+ * @author t3rik
+ * @date 2024-07-01
+ */
+@Mapper
+public interface SysConfigJasyptMapper extends BaseMapper<SysConfigJasypt> {
+
+}
