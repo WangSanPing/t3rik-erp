2b8e9ccbb28ea878b9569a4e94cca861807dfe08
t3rik <t3rik@itcast.cn>
2024-07-05 21:21:49 +0800

    feat: 1.重构添加生产任务代码 2.生产任务表添加客户订单字段 3.移动端修改获取列表的方法

--- t3rik-mes/src/main/java/com/t3rik/mes/pro/domain/ProTask.java
+++ t3rik-mes/src/main/java/com/t3rik/mes/pro/domain/ProTask.java
@@ -1,15 +1,14 @@
 package com.t3rik.mes.pro.domain;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
-import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.t3rik.common.annotation.Excel;
 import com.t3rik.common.core.domain.BaseEntity;
 import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * 生产任务对象 pro_task
@@ -22,14 +21,20 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class ProTask extends BaseEntity
-{
+public class ProTask extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
-    /** 任务ID */
+    /**
+     * 任务ID
+     */
     @TableId
     private Long taskId;
 
+    /**
+     * 客户订单号
+     */
+    @Excel(name = "客户订单号")
+    private String clientOrderCode;
 
     /**
      * 查询页
@@ -38,35 +43,51 @@ public class ProTask extends BaseEntity
     @TableField(exist = false)
     private String currentIndex;
 
-    /** 任务编号 */
+    /**
+     * 任务编号
+     */
     @Excel(name = "任务编号")
     private String taskCode;
 
-    /** 任务名称 */
+    /**
+     * 任务名称
+     */
     @Excel(name = "任务名称")
     private String taskName;
 
-    /** 生产工单ID */
+    /**
+     * 生产工单ID
+     */
     @Excel(name = "生产工单ID")
     private Long workorderId;
 
-    /** 生产工单编号 */
+    /**
+     * 生产工单编号
+     */
     @Excel(name = "生产工单编号")
     private String workorderCode;
 
-    /** 工单名称 */
+    /**
+     * 工单名称
+     */
     @Excel(name = "工单名称")
     private String workorderName;
 
-    /** 工作站ID */
+    /**
+     * 工作站ID
+     */
     @Excel(name = "工作站ID")
     private Long workstationId;
 
-    /** 工作站编号 */
+    /**
+     * 工作站编号
+     */
     @Excel(name = "工作站编号")
     private String workstationCode;
 
-    /** 工作站名称 */
+    /**
+     * 工作站名称
+     */
     @Excel(name = "工作站名称")
     private String workstationName;
 
@@ -74,45 +95,65 @@ public class ProTask extends BaseEntity
 
     private String routeCode;
 
-    /** 工序ID */
+    /**
+     * 工序ID
+     */
     @Excel(name = "工序ID")
     private Long processId;
 
-    /** 工序编码 */
+    /**
+     * 工序编码
+     */
     @Excel(name = "工序编码")
     private String processCode;
 
-    /** 工序名称 */
+    /**
+     * 工序名称
+     */
     @Excel(name = "工序名称")
     private String processName;
 
     private String isCheck;
 
-    /** 产品物料ID */
+    /**
+     * 产品物料ID
+     */
     @Excel(name = "产品物料ID")
     private Long itemId;
 
-    /** 产品物料编码 */
+    /**
+     * 产品物料编码
+     */
     @Excel(name = "产品物料编码")
     private String itemCode;
 
-    /** 产品物料名称 */
+    /**
+     * 产品物料名称
+     */
     @Excel(name = "产品物料名称")
     private String itemName;
 
-    /** 规格型号 */
+    /**
+     * 规格型号
+     */
     @Excel(name = "规格型号")
     private String specification;
 
-    /** 单位 */
+    /**
+     * 单位
+     */
     @Excel(name = "单位")
     private String unitOfMeasure;
 
-    /** 排产数量 */
+    /**
+     * 排产数量
+     */
     @Excel(name = "排产数量")
     private BigDecimal quantity;
 
-    /** 已生产数量 */
+    /**
+     * 已生产数量
+     */
     @Excel(name = "已生产数量")
     private BigDecimal quantityProduced;
 
@@ -120,61 +161,89 @@ public class ProTask extends BaseEntity
 
     private BigDecimal quantityUnquanlify;
 
-    /** 调整数量 */
+    /**
+     * 调整数量
+     */
     @Excel(name = "调整数量")
     private BigDecimal quantityChanged;
 
-    /** 客户ID */
+    /**
+     * 客户ID
+     */
     @Excel(name = "客户ID")
     private Long clientId;
 
-    /** 客户编码 */
+    /**
+     * 客户编码
+     */
     @Excel(name = "客户编码")
     private String clientCode;
 
-    /** 客户名称 */
+    /**
+     * 客户名称
+     */
     @Excel(name = "客户名称")
     private String clientName;
 
-    /** 客户简称 */
+    /**
+     * 客户简称
+     */
     @Excel(name = "客户简称")
     private String clientNick;
 
-    /** 开始生产时间 */
+    /**
+     * 开始生产时间
+     */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Excel(name = "开始生产时间", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
     private Date startTime;
 
-    /** 生产时长 */
+    /**
+     * 生产时长
+     */
     @Excel(name = "生产时长")
     private Long duration;
 
-    /** 完成生产时间 */
+    /**
+     * 完成生产时间
+     */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Excel(name = "完成生产时间", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
     private Date endTime;
 
-    /** 甘特图显示颜色 */
+    /**
+     * 甘特图显示颜色
+     */
     @Excel(name = "甘特图显示颜色")
     private String colorCode;
 
-    /** 需求日期 */
+    /**
+     * 需求日期
+     */
     @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     @Excel(name = "需求日期", width = 30, dateFormat = "yyyy-MM-dd")
     private Date requestDate;
 
     private String status;
 
-    /** 预留字段1 */
+    /**
+     * 预留字段1
+     */
     private String attr1;
 
-    /** 预留字段2 */
+    /**
+     * 预留字段2
+     */
     private String attr2;
 
-    /** 预留字段3 */
+    /**
+     * 预留字段3
+     */
     private Long attr3;
 
-    /** 预留字段4 */
+    /**
+     * 预留字段4
+     */
     private Long attr4;
 
     /**
