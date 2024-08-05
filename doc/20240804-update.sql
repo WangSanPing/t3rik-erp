a0a1e48cad2d444125f627755d348d97073a20c5
t3rik <t3rik@itcast.cn>
2024-08-05 23:09:42 +0800

    feat: 1.上报工时的同时校验当前是否已经超出排产数量，如果超出，会自动完成任务

---
+++ doc/20240804-update.sql
@@ -0,0 +1,6 @@
+-- 更新图标
+UPDATE `sys_menu` SET  `icon` = '/static/icons/u1193.svg' WHERE `menu_id` = 2342;
+UPDATE `sys_menu` SET  `icon` = '/static/icons/u1175.svg' WHERE `menu_id` = 2343;
+UPDATE `sys_menu` SET  `icon` = '/static/icons/u1199.svg' WHERE `menu_id` = 2344;
+UPDATE `sys_menu` SET  `icon` = '/static/icons/u1196.svg' WHERE `menu_id` = 2345;
+UPDATE `sys_menu` SET  `icon` = '/static/icons/u1181.svg' WHERE `menu_id` = 2346;
--- t3rik-mobile/src/main/kotlin/com/t3rik/mobile/common/ktextend/BigDecimalExtend.kt
+++ t3rik-mobile/src/main/kotlin/com/t3rik/mobile/common/ktextend/BigDecimalExtend.kt
@@ -14,3 +14,25 @@ import java.math.BigDecimal
 fun BigDecimal.isZero(): Boolean {
     return this.compareTo(BigDecimal.ZERO) == 0
 }
+
+/**
+ * 判断a是否等于b
+ */
+fun BigDecimal.isGreater(value: BigDecimal): Boolean {
+    return this > value
+}
+
+/**
+ * 判断a是否大于等于b
+ */
+fun BigDecimal.isGreaterOrEqual(value: BigDecimal): Boolean {
+    return this >= value
+}
+
+/**
+ * 判断数值是否为null，如果为null，返回0
+ * 不为null 返回当前数值
+ */
+fun BigDecimal?.orZero(): BigDecimal {
+    return this ?: BigDecimal.ZERO
+}
