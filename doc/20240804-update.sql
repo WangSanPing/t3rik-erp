a28740b267f7737d651cd762650ff175c153c570
t3rik <t3rik@itcast.cn>
2024-08-07 19:54:26 +0800

    feat: 1.添加更新数据库字段的sql

--- doc/20240804-update.sql
+++ doc/20240804-update.sql
@@ -4,3 +4,8 @@ UPDATE `sys_menu` SET  `icon` = '/static/icons/u1175.svg' WHERE `menu_id` = 2343
 UPDATE `sys_menu` SET  `icon` = '/static/icons/u1199.svg' WHERE `menu_id` = 2344;
 UPDATE `sys_menu` SET  `icon` = '/static/icons/u1196.svg' WHERE `menu_id` = 2345;
 UPDATE `sys_menu` SET  `icon` = '/static/icons/u1181.svg' WHERE `menu_id` = 2346;
+
+
+-- 添加字段
+ALTER TABLE wm_item_recpt ADD total_amount decimal(12,4) COMMENT '总金额';
+ALTER TABLE wm_item_recpt_line ADD amount decimal(12,4) COMMENT '金额';
