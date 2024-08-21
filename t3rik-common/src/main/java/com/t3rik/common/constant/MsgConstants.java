1a93594a21dbc31ce2032c51a2d30221809fb595
t3rik <t3rik@itcast.cn>
2024-08-21 14:07:15 +0800

    feat: 完成移动端审核页面

--- t3rik-common/src/main/java/com/t3rik/common/constant/MsgConstants.java
+++ t3rik-common/src/main/java/com/t3rik/common/constant/MsgConstants.java
@@ -16,7 +16,11 @@ public class MsgConstants {
 
     public static final String CAN_NOT_BE_ZERO  = "数量不许为0!";
 
+    public static final String SELECT_AT_LEAST_ONE  = "请至少选择一项!";
+
     public static final String NOT_EXIST_WORKSTATION  = "当前生产任务对应的工作站不存在!";
 
     public static final String ERROR_ROUTE  = "当前任务对应的工艺和工序配置无效!";
+
+    public static final String NO_AUDIT_AUTH  = "当前用户没有审核权限!";
 }
