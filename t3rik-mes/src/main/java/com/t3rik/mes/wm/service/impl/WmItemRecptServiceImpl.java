9bed77d41b99f1721bf4cf37132d4f9567d4008f
t3rik <wangx452@qq.com>
2024-08-27 19:57:08 +0800

    perf: 1.重构退料controller

--- t3rik-mes/src/main/java/com/t3rik/mes/wm/service/impl/WmItemRecptServiceImpl.java
+++ t3rik-mes/src/main/java/com/t3rik/mes/wm/service/impl/WmItemRecptServiceImpl.java
@@ -2,14 +2,17 @@ package com.t3rik.mes.wm.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.t3rik.common.constant.UserConstants;
+import com.t3rik.common.enums.mes.OrderStatusEnum;
 import com.t3rik.common.utils.DateUtils;
 import com.t3rik.common.utils.StringUtils;
 import com.t3rik.mes.wm.domain.WmItemRecpt;
 import com.t3rik.mes.wm.domain.tx.ItemRecptTxBean;
 import com.t3rik.mes.wm.mapper.WmItemRecptMapper;
+import com.t3rik.mes.wm.service.IStorageCoreService;
 import com.t3rik.mes.wm.service.IWmItemRecptService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,8 +24,11 @@ import java.util.List;
  */
 @Service
 public class WmItemRecptServiceImpl extends ServiceImpl<WmItemRecptMapper, WmItemRecpt> implements IWmItemRecptService {
-    @Autowired
+
+    @Resource
     private WmItemRecptMapper wmItemRecptMapper;
+    @Resource
+    private IStorageCoreService storageCoreService;
 
     /**
      * 查询物料入库单
@@ -105,5 +111,25 @@ public class WmItemRecptServiceImpl extends ServiceImpl<WmItemRecptMapper, WmIte
     @Override
     public List<ItemRecptTxBean> getTxBeans(Long receptId) {
         return wmItemRecptMapper.getTxBeans(receptId);
+    }
+
+    /**
+     * 执行
+     *
+     * @param recptId
+     */
+    @Transactional
+    @Override
+    public void execute(Long recptId) {
+
+        // 构造Transaction事务，并执行库存更新逻辑
+        List<ItemRecptTxBean> beans = this.getTxBeans(recptId);
+        // 调用库存核心
+        storageCoreService.processItemRecpt(beans);
+        // 更新单据状态
+        this.lambdaUpdate()
+                .set(WmItemRecpt::getStatus, OrderStatusEnum.FINISHED.getCode())
+                .eq(WmItemRecpt::getRecptId, recptId)
+                .update(new WmItemRecpt());
     }
 }
