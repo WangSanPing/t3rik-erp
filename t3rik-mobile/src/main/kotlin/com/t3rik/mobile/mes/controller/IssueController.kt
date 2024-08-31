7e45b3bea264022b5a584209c38f71bc4f09ae81
t3rik <wangx452@qq.com>
2024-08-31 22:58:33 +0800

    feat: 完成领料申请的接口定义

--- t3rik-mobile/src/main/kotlin/com/t3rik/mobile/mes/controller/IssueController.kt
+++ t3rik-mobile/src/main/kotlin/com/t3rik/mobile/mes/controller/IssueController.kt
@@ -3,15 +3,20 @@ package com.t3rik.mobile.mes.controller
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper
 import com.t3rik.common.constant.UserConstants
 import com.t3rik.common.core.controller.BaseController
+import com.t3rik.common.core.domain.AjaxResult
 import com.t3rik.common.core.page.TableDataInfo
 import com.t3rik.common.utils.SecurityUtils
 import com.t3rik.common.utils.StringUtils
 import com.t3rik.mes.pro.domain.ProTask
 import com.t3rik.mes.pro.dto.TaskDto
 import com.t3rik.mes.pro.service.IProTaskService
+import com.t3rik.mobile.mes.dto.IssueRequestDTO
 import io.swagger.annotations.ApiOperation
 import jakarta.annotation.Resource
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -39,5 +44,12 @@ class IssueController : BaseController() {
         queryWrapper.likeRight(StringUtils.isNotBlank(task.taskName), "task_name", task.taskName)
         queryWrapper.eq("task_user_id", SecurityUtils.getUserId())
         return getDataTableWithPage(this.proTaskService.getTaskListAndIssueCount(page, queryWrapper))
+    }
+
+    @ApiOperation("领料申请")
+    @PostMapping
+    fun issue(@RequestBody issueRequestDTO: IssueRequestDTO):AjaxResult{
+        logger.info(issueRequestDTO.toString())
+        return AjaxResult.success()
     }
 }
