3da1bde70056b4bba8ea4fdc9cb217f2e6bbf1f6
t3rik <wangx452@qq.com>
2024-09-21 21:15:10 +0800

    feat: update-sql

--- doc/20240903-update.sql
+++
@@ -1,3 +0,0 @@
--- wm表添加创建人和修改人id
-ALTER TABLE wm_material_stock ADD COLUMN create_user_id BIGINT COMMENT '创建人id',ADD COLUMN update_user_id BIGINT COMMENT '修改人id';
-ALTER TABLE wm_material_stock ADD COLUMN remark varchar(500) COMMENT '备注';
