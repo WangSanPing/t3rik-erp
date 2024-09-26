7e62d54b51ec05c133cf6b7e2959a5271863bea7
t3rik <wangx452@qq.com>
2024-09-26 14:47:39 +0800

    perf: 代码review

--- t3rik-mes/src/main/java/com/t3rik/mes/sales/service/impl/SalesOrderServiceImpl.java
+++ t3rik-mes/src/main/java/com/t3rik/mes/sales/service/impl/SalesOrderServiceImpl.java
@@ -1,16 +1,15 @@
 package com.t3rik.mes.sales.service.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollectionUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.t3rik.common.constant.UserConstants;
-import com.t3rik.common.core.domain.AjaxResult;
-import com.t3rik.common.enums.mes.*;
+import com.t3rik.common.enums.mes.ItemTypeEnum;
+import com.t3rik.common.enums.mes.OrderStatusEnum;
+import com.t3rik.common.enums.mes.WorkOrderSourceTypeEnum;
+import com.t3rik.common.enums.mes.WorkOrderTypeEnum;
 import com.t3rik.common.utils.StringUtils;
 import com.t3rik.mes.api.service.IMesWorkOrderService;
 import com.t3rik.mes.common.service.IAsyncService;
@@ -22,20 +21,23 @@ import com.t3rik.mes.pro.domain.ProWorkorderBom;
 import com.t3rik.mes.pro.service.IProClientOrderItemService;
 import com.t3rik.mes.pro.service.IProWorkorderBomService;
 import com.t3rik.mes.pro.service.IProWorkorderService;
+import com.t3rik.mes.sales.domain.SalesOrder;
 import com.t3rik.mes.sales.domain.SalesOrderItem;
+import com.t3rik.mes.sales.mapper.SalesOrderMapper;
 import com.t3rik.mes.sales.service.ISalesOrderItemService;
+import com.t3rik.mes.sales.service.ISalesOrderService;
 import com.t3rik.mes.wm.domain.WmMaterialStock;
 import com.t3rik.mes.wm.service.IWmMaterialStockService;
 import com.t3rik.system.strategy.AutoCodeUtil;
 import jakarta.annotation.Resource;
-import org.checkerframework.checker.units.qual.A;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.t3rik.mes.sales.mapper.SalesOrderMapper;
-import com.t3rik.mes.sales.domain.SalesOrder;
-import com.t3rik.mes.sales.service.ISalesOrderService;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 销售订单Service业务层处理
@@ -44,8 +46,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @date 2024-08-29
  */
 @Service
-public class SalesOrderServiceImpl  extends ServiceImpl<SalesOrderMapper, SalesOrder>  implements ISalesOrderService
-{
+public class SalesOrderServiceImpl extends ServiceImpl<SalesOrderMapper, SalesOrder> implements ISalesOrderService {
     @Autowired
     private SalesOrderMapper salesOrderMapper;
     @Autowired
@@ -69,49 +70,50 @@ public class SalesOrderServiceImpl  extends ServiceImpl<SalesOrderMapper, SalesO
 
     @Override
     @Transactional
-    public boolean saveOrder(SalesOrder salesOrder){
-        if(salesOrder.getSalesOrderItemList().size()>0){
+    public boolean saveOrder(SalesOrder salesOrder) {
+        if (salesOrder.getSalesOrderItemList().size() > 0) {
             this.save(salesOrder);
-            this.buildSalesOrderItems(salesOrder).stream().forEach(f->{
-                if(f.getSalesOrderDate()==null){
+            this.buildSalesOrderItems(salesOrder).stream().forEach(f -> {
+                if (f.getSalesOrderDate() == null) {
                     f.setSalesOrderDate(salesOrder.getSalesOrderDate());
                 }
-                if(f.getDeliveryDate()==null){
+                if (f.getDeliveryDate() == null) {
                     f.setDeliveryDate(salesOrder.getDeliveryDate());
                 }
                 this.salesOrderItemService.save(f);
                 this.buildItemProduct(f);
             });
             return true;
-        }else{
+        } else {
             this.save(salesOrder);
             return true;
         }
     }
+
     /**
      * 构建订单子项
      */
     @NotNull
-    private List<SalesOrderItem> buildSalesOrderItems(SalesOrder salesOrder){
-        List<SalesOrderItem>itemList=new ArrayList<>();
-        for(SalesOrderItem obj:salesOrder.getSalesOrderItemList()){
+    private List<SalesOrderItem> buildSalesOrderItems(SalesOrder salesOrder) {
+        List<SalesOrderItem> itemList = new ArrayList<>();
+        for (SalesOrderItem obj : salesOrder.getSalesOrderItemList()) {
             obj.setSalesOrderId(salesOrder.getSalesOrderId());
             obj.setSalesOrderCode(salesOrder.getSalesOrderCode());
             obj.setOweQty(obj.getSalesOrderQuantity());
             obj.setSalesOrderItemCode(autoCodeUtil.genSerialCode("SALES_ITEM_CODE", null));
-            if(salesOrder.getStatus()!=null){
+            if (salesOrder.getStatus() != null) {
                 obj.setStatus(salesOrder.getStatus());
             }
-            //查询库存
-            WmMaterialStock wmMaterialStock=new WmMaterialStock();
+            // 查询库存
+            WmMaterialStock wmMaterialStock = new WmMaterialStock();
             wmMaterialStock.setItemId(obj.getProductId());
             wmMaterialStock.setItemCode(obj.getProductCode());
             wmMaterialStock.setItemName(obj.getProductName());
             List<WmMaterialStock> list = wmMaterialStockService.selectWmMaterialStockList(wmMaterialStock);
-            if(list.size()>0){
-                StringBuffer sb=new StringBuffer();
-                list.forEach(f->{
-                    sb.append(f.getWarehouseName()+":"+f.getQuantityOnhand());
+            if (list.size() > 0) {
+                StringBuffer sb = new StringBuffer();
+                list.forEach(f -> {
+                    sb.append(f.getWarehouseName() + ":" + f.getQuantityOnhand());
                 });
                 obj.setStockNum(sb.toString());
             }
@@ -125,7 +127,7 @@ public class SalesOrderServiceImpl  extends ServiceImpl<SalesOrderMapper, SalesO
     /**
      * 订单子项产品是否有bom
      */
-    private void buildItemProduct(SalesOrderItem salesOrderItem){
+    private void buildItemProduct(SalesOrderItem salesOrderItem) {
         // 查询当前产品是否存在bom组合,如果存在,写入到客户订单物料表中
         List<MdProductBom> productBoms = productBomService.lambdaQuery().
                 eq(MdProductBom::getItemId, salesOrderItem.getProductId())
@@ -137,6 +139,7 @@ public class SalesOrderServiceImpl  extends ServiceImpl<SalesOrderMapper, SalesO
             this.clientOrderItemService.saveBatch(proClientOrderItems);
         }
     }
+
     /**
      * 构建客户订单子项
      */
@@ -169,12 +172,12 @@ public class SalesOrderServiceImpl  extends ServiceImpl<SalesOrderMapper, SalesO
      */
     @Transactional
     @Override
-    public  List<ProWorkorder>  execute(SalesOrder salesOrder) throws Exception {
-        List<SalesOrderItem> salesOrderItems=salesOrder.getSalesOrderItemList();
-        List<ProWorkorder> proWorkorderList=new ArrayList<>();
-        salesOrderItems.stream().forEach(f ->{
+    public List<ProWorkorder> execute(SalesOrder salesOrder) throws Exception {
+        List<SalesOrderItem> salesOrderItems = salesOrder.getSalesOrderItemList();
+        List<ProWorkorder> proWorkorderList = new ArrayList<>();
+        salesOrderItems.stream().forEach(f -> {
             // 生产订单
-            ProWorkorder workorder = buildWorkOrder(f,salesOrder.getOrderType());
+            ProWorkorder workorder = buildWorkOrder(f, salesOrder.getOrderType());
             // 保存生成工单
             this.proWorkorderService.save(workorder);
             // 查询对应的客户订单子项
@@ -199,7 +202,7 @@ public class SalesOrderServiceImpl  extends ServiceImpl<SalesOrderMapper, SalesO
                     .update();
             // 异步回写产品对应的bom列表
             this.asyncService.updateItemBomAndLevel(f.getProductId(), clientOrderItems);
-            proWorkorderList.add(workorder) ;
+            proWorkorderList.add(workorder);
 
         });
         salesOrder.setStatus("WORK_ORDER_FINISHED");
@@ -218,11 +221,11 @@ public class SalesOrderServiceImpl  extends ServiceImpl<SalesOrderMapper, SalesO
 
         this.updateById(salesOrder);
         if (salesOrder.getSalesOrderItemList().size() > 0) {
-            this.buildSalesOrderItems(salesOrder).stream().forEach(f->{
-                if(f.getSalesOrderDate()==null){
+            this.buildSalesOrderItems(salesOrder).stream().forEach(f -> {
+                if (f.getSalesOrderDate() == null) {
                     f.setSalesOrderDate(salesOrder.getSalesOrderDate());
                 }
-                if(f.getDeliveryDate()==null){
+                if (f.getDeliveryDate() == null) {
                     f.setDeliveryDate(salesOrder.getDeliveryDate());
                 }
                 this.salesOrderItemService.save(f);
@@ -234,22 +237,51 @@ public class SalesOrderServiceImpl  extends ServiceImpl<SalesOrderMapper, SalesO
 
     @Override
     @Transactional
-    public StringBuffer deleteByIds(List<Long> salesOrderIds){
+    public StringBuffer deleteByIds(List<Long> salesOrderIds) {
         List<SalesOrder> salesOrders = this.listByIds(salesOrderIds);
+        // -t3rik
+        // 没有线程安全的问题的情况下 用 StringBuilder
         StringBuffer sb = new StringBuffer();
         for (SalesOrder li : salesOrders) {
             List<SalesOrderItem> orderItemList = getItemList(li);
+            // -t3rik
+            // 建议多用工具类做集合判空和非空，例如 if(CollectionUtils.isNotEmpty())
             if (orderItemList.size() > 0) {
+                // 已经用了StringBuffer类了，就不要再用字符串+号拼接了
+                // sb.append("销售订单").append(li.getSalesOrderCode()).append("下还有未删除的清单，不允许删除").append("\n");
                 sb.append("销售订单" + li.getSalesOrderCode() + "下还有未删除的清单，不允许删除" + "\n");
                 salesOrderIds.remove(li.getSalesOrderId());
             }
         }
         this.removeByIds(salesOrderIds);
+
+        // 参考
+        // List<SalesOrder> salesOrders = this.listByIds(salesOrderIds);
+        // if (CollectionUtils.isEmpty(salesOrders)) {
+        //     return new StringBuffer("没有要删除的数据!");
+        // }
+        // // 返回错误信息
+        // StringBuffer sb = new StringBuffer();
+        // salesOrders.forEach(item -> {
+        //     List<SalesOrderItem> orderItemList =
+        //             this.salesOrderItemService.lambdaQuery()
+        //                     .eq(SalesOrderItem::getSalesOrderId, item.getSalesOrderId())
+        //                     .list();
+        //     // 没数据后面就不操作了，直接到下个循环
+        //     if (CollectionUtils.isEmpty(orderItemList)) {
+        //         return;
+        //     }
+        //     sb.append("销售订单").append(item.getSalesOrderCode()).append("下还有未删除的清单，不允许删除").append("\n");
+        //     salesOrderIds.remove(item.getSalesOrderId());
+        // });
+        // this.removeByIds(salesOrderIds);
         return sb;
     }
 
-    //根据主单id查找子项
-    private List<SalesOrderItem> getItemList(SalesOrder salesOrder){
+    // 根据主单id查找子项
+    // -t3rik
+    // 这个可以写成一行，提这个方法没有太大不要
+    private List<SalesOrderItem> getItemList(SalesOrder salesOrder) {
         LambdaQueryWrapper<SalesOrderItem> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(SalesOrderItem::getSalesOrderId, salesOrder.getSalesOrderId());
         List<SalesOrderItem> orderItemList = salesOrderItemService.list(queryWrapper);
@@ -258,19 +290,36 @@ public class SalesOrderServiceImpl  extends ServiceImpl<SalesOrderMapper, SalesO
 
     @Override
     @Transactional
-    public boolean refuse(SalesOrder salesOrder){
+    public boolean refuse(SalesOrder salesOrder) {
         // 审批拒绝/提交
         this.updateById(salesOrder);
         List<SalesOrderItem> itemList = salesOrderItemService.lambdaQuery()
                 .eq(SalesOrderItem::getSalesOrderId, salesOrder.getSalesOrderId())
                 .list();
+        // -t3rik
+        // 参考
+        // 这里如果itemList为空应该可以直接返回了。
+        // if(CollectionUtils.isEmpty(itemList)){
+        //     return false;
+        // }
+        // // 不为空再执行下面的
+        // salesOrder.setSalesOrderItemList(itemList);
+        // salesOrder.getSalesOrderItemList().forEach(object -> object.setStatus(salesOrder.getStatus()));
+        // return salesOrderItemService.updateBatchById(salesOrder.getSalesOrderItemList());
+        // 但是我不确定你的逻辑，如果SalesOrderItemList要是没有需要更新的子单，就算更新失败的话，那你直接返回false，不会触发事务
+        // 要是你主单和子单都各自更新的话，返回false就没问题，
+        // 要想触发事务，就得抛异常
         salesOrder.setSalesOrderItemList(itemList);
         if (salesOrder.getSalesOrderItemList().size() > 0) {
             salesOrder.getSalesOrderItemList().forEach(object -> object.setStatus(salesOrder.getStatus()));
-        }else{
+        } else {
             return false;
         }
         return salesOrderItemService.updateBatchById(salesOrder.getSalesOrderItemList());
+
+
+
+
     }
 
     /**
@@ -310,7 +359,7 @@ public class SalesOrderServiceImpl  extends ServiceImpl<SalesOrderMapper, SalesO
     /**
      * 构建生产订单
      */
-    private @NotNull ProWorkorder buildWorkOrder(@NotNull SalesOrderItem salesOrderItem,String sourceCode) {
+    private @NotNull ProWorkorder buildWorkOrder(@NotNull SalesOrderItem salesOrderItem, String sourceCode) {
         ProWorkorder workorder = new ProWorkorder();
         BeanUtil.copyProperties(salesOrderItem, workorder);
         String workorderCode = this.autoCodeUtil.genSerialCode(UserConstants.WORKORDER_CODE, null);
