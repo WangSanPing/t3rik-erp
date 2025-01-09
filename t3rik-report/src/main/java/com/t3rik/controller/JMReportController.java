d3bf428d618b9fb6fd73edd553ead4a6c8cf9faa
t3rik <wangx452@qq.com>
2025-01-10 00:55:50 +0800

    fix: 修复服务器环境无法访问图表的问题

--- t3rik-report/src/main/java/com/t3rik/controller/JMReportController.java
+++ t3rik-report/src/main/java/com/t3rik/controller/JMReportController.java
@@ -18,7 +18,15 @@ public class JMReportController {
         String scheme = request.getScheme(); // http 或 https
         String serverName = request.getServerName(); // 主机名或 IP
         int serverPort = request.getServerPort(); // 端口号
-        String url = scheme + "://" + serverName + ":" + serverPort + "/jmreport/list";
+        // 检验是否是服务器中通过java -jar运行，如果是，需要加入nginx转发路径
+        String classPath = System.getProperty("java.class.path");
+        String url;
+        if (classPath.contains(".jar")) {
+            url = scheme + "://" + serverName + ":" + serverPort + "/prod-api/jmreport/list";
+        } else {
+            url = scheme + "://" + serverName + ":" + serverPort + "/jmreport/list";
+        }
+
         return AjaxResult.success(url);
     }
 }
