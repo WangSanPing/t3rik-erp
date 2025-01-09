a15a181ebf1f99d898050ee91644f64ba22391fe
t3rik <wangx452@qq.com>
2025-01-10 00:22:31 +0800

    feat: 集成积木报表

---
+++ t3rik-report/src/main/java/com/t3rik/controller/JMReportController.java
@@ -0,0 +1,24 @@
+package com.t3rik.controller;
+
+import com.t3rik.common.core.domain.AjaxResult;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author t3rik
+ * @date 2025/1/10 00:10
+ */
+@RestController
+@RequestMapping("/t3rik/jmreport")
+public class JMReportController {
+    @GetMapping
+    public AjaxResult getJMReportUrl(HttpServletRequest request) {
+        String scheme = request.getScheme(); // http 或 https
+        String serverName = request.getServerName(); // 主机名或 IP
+        int serverPort = request.getServerPort(); // 端口号
+        String url = scheme + "://" + serverName + ":" + serverPort + "/jmreport/list";
+        return AjaxResult.success(url);
+    }
+}
