89d71668316a8ba3f017b430c894b948ae173d91
t3rik <wangx452@qq.com>
2025-01-10 01:27:19 +0800

    fix: 修复服务器环境无法访问图表的问题

--- t3rik-report/src/main/java/com/t3rik/controller/JMReportController.java
+++ t3rik-report/src/main/java/com/t3rik/controller/JMReportController.java
@@ -3,6 +3,7 @@ package com.t3rik.controller;
 import com.t3rik.common.core.domain.AjaxResult;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author t3rik
  * @date 2025/1/10 00:10
  */
+@Slf4j
 @RestController
 @RequestMapping("/t3rik/jmreport")
 public class JMReportController {
@@ -27,11 +29,12 @@ public class JMReportController {
         int serverPort = request.getServerPort(); // 端口号
         String[] activeProfiles = environment.getActiveProfiles();
         String url;
-        if(activeProfiles[0].equals("pro")){
+        if (activeProfiles[0].equals("pro")) {
             url = "https://" + serverName + ":" + serverPort + "/prod-api/jmreport/list";
-        }else {
+        } else {
             url = scheme + "://" + serverName + ":" + serverPort + "/jmreport/list";
         }
+        log.info("当前访问的报表地址: {}", url);
         return AjaxResult.success(url);
     }
 }
