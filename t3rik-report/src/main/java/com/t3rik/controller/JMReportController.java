ac7c9fa3e40b0581d91bebe16cd6f51e84f94606
t3rik <wangx452@qq.com>
2025-01-10 01:20:01 +0800

    fix: 修复服务器环境无法访问图表的问题

--- t3rik-report/src/main/java/com/t3rik/controller/JMReportController.java
+++ t3rik-report/src/main/java/com/t3rik/controller/JMReportController.java
@@ -1,7 +1,9 @@
 package com.t3rik.controller;
 
 import com.t3rik.common.core.domain.AjaxResult;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,20 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/t3rik/jmreport")
 public class JMReportController {
+
+    @Resource
+    private Environment environment;
+
+
     @GetMapping
     public AjaxResult getJMReportUrl(HttpServletRequest request) {
         String scheme = request.getScheme(); // http 或 https
         String serverName = request.getServerName(); // 主机名或 IP
         int serverPort = request.getServerPort(); // 端口号
-        // 检验是否是服务器中通过java -jar运行，如果是，需要加入nginx转发路径
-        String classPath = System.getProperty("java.class.path");
+        String[] activeProfiles = environment.getActiveProfiles();
         String url;
-        if (classPath.contains(".jar")) {
+        if(activeProfiles[0].equals("pro")){
             url = scheme + "://" + serverName + ":" + serverPort + "/prod-api/jmreport/list";
-        } else {
+        }else {
             url = scheme + "://" + serverName + ":" + serverPort + "/jmreport/list";
         }
-
         return AjaxResult.success(url);
     }
 }
