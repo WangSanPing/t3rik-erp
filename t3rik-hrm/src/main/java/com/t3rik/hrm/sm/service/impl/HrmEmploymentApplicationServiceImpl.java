e78f881c82cc5a72ef29a408593c6da01366661a
t3rik <wangx452@qq.com>
2025-01-13 23:56:51 +0800

    feat: 调整员工审核通过入职后生成账号

--- t3rik-hrm/src/main/java/com/t3rik/hrm/sm/service/impl/HrmEmploymentApplicationServiceImpl.java
+++ t3rik-hrm/src/main/java/com/t3rik/hrm/sm/service/impl/HrmEmploymentApplicationServiceImpl.java
@@ -20,6 +20,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.Optional;
 
 /**
@@ -61,45 +62,51 @@ public class HrmEmploymentApplicationServiceImpl implements IHrmEmploymentApplic
     @Override
     public String pass(HrmInterviewRecord hrmInterviewRecord, StaffState state) {
         // 写入新的面试记录
-        saveNewInterviewRecord(hrmInterviewRecord, state);
+        HrmInterviewRecord newRecord = saveNewInterviewRecord(hrmInterviewRecord, state);
+        this.interviewRecordService.save(newRecord);
+        // 创建该员工的账户
+        SysUser user = buildSysUser(hrmInterviewRecord.getStaffId(), hrmInterviewRecord);
+        this.sysUserService.insertUser(user);
         // 修改员工状态
         this.hrmStaffService.lambdaUpdate()
                 .set(HrmStaff::getStatus, state.getCurrentStatus())
                 .eq(HrmStaff::getStaffId, hrmInterviewRecord.getStaffId())
                 .update(new HrmStaff());
-        // 创建该员工的账户
-        SysUser user = buildSysUser(hrmInterviewRecord.getStaffId());
-
-
-        return "";
+        return "当前已生成员工账号";
     }
 
     /**
      * 构建用户信息
      */
-    private SysUser buildSysUser(Long staffId) {
+    private SysUser buildSysUser(Long staffId, HrmInterviewRecord hrmInterviewRecord) {
         HrmStaff staff = this.hrmStaffService.getById(staffId);
         String account = PinyinUtil.getFirstLetter(staff.getStaffName(), "") + StringUtils.right(staff.getContactPhone(), 4);
         SysUser user = new SysUser();
         user.setUserName(account);
+        user.setNickName(staff.getStaffName());
         user.setPassword(SecurityUtils.encryptPassword(UserConstants.DEFAULT_PASSWORD));
         user.setPhonenumber(staff.getContactPhone());
         user.setSex(staff.getSex() != null ? staff.getSex().toString() : SexEnum.UNKNOWN.getCode());
         user.setEmail(staff.getEmail());
         user.setStatus(StatusEnum.ENABLE.getCode());
+        user.setRoleIds(new Long[]{hrmInterviewRecord.getRoleId()});
+        user.setPostIds(new Long[]{hrmInterviewRecord.getPostId()});
+        user.setDeptId(hrmInterviewRecord.getDeptId());
+        user.setCreateBy(SecurityUtils.getUserId().toString());
+        user.setCreateTime(new Date());
         return user;
     }
 
     /**
      * 新面试记录
      */
-    private void saveNewInterviewRecord(HrmInterviewRecord hrmInterviewRecord, StaffState state) {
+    private HrmInterviewRecord saveNewInterviewRecord(HrmInterviewRecord hrmInterviewRecord, StaffState state) {
         HrmInterviewRecord newRecord = this.interviewRecordService.getById(hrmInterviewRecord.getInterviewRecordId());
         Optional.ofNullable(newRecord).orElseThrow(() -> new BusinessException(MsgConstants.ERROR_DATA));
         // 写入新纪录
         newRecord.setRemark(hrmInterviewRecord.getRemark());
         newRecord.setInterviewRecordId(null);
         newRecord.setStatus(state.getCurrentStatus().longValue());
-        this.interviewRecordService.save(newRecord);
+        return newRecord;
     }
 }
