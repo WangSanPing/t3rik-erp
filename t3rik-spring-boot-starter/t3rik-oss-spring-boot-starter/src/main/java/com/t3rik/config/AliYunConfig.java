144e76d65a6bb0f2ad7c2c34a556597a95b7fcc9
t3rik <wangx452@qq.com>
2025-01-19 17:16:13 +0800

    fix: 解决如果缺失oss配置，无法启动的问题

--- t3rik-spring-boot-starter/t3rik-oss-spring-boot-starter/src/main/java/com/t3rik/config/AliYunConfig.java
+++ t3rik-spring-boot-starter/t3rik-oss-spring-boot-starter/src/main/java/com/t3rik/config/AliYunConfig.java
@@ -2,6 +2,7 @@ package com.t3rik.config;
 
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
+import com.t3rik.enums.OssTypeEnum;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,10 @@ public class AliYunConfig {
 
     @Bean
     public OSS ossClient(OssProperties ossProperties) {
+        // 非aliyun配置 不初始化客户端
+        if (!ossProperties.getOssType().equals(OssTypeEnum.AliYun)) {
+            return null;
+        }
         return new OSSClientBuilder()
                 .build(ossProperties.getEndPoint(), ossProperties.getAccessKey(), ossProperties.getSecretKey());
     }
