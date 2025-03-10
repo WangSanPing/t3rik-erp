804be1d08ff0826360780d5634d8aa9a56bb5a40
t3rik <wangx452@qq.com>
2025-03-10 22:25:21 +0800

    feat: 添加查询退料任务接口

--- t3rik-mobile/src/main/kotlin/com/t3rik/mobile/mes/controller/RtIssueController.kt
+++ t3rik-mobile/src/main/kotlin/com/t3rik/mobile/mes/controller/RtIssueController.kt
@@ -1,10 +1,16 @@
 package com.t3rik.mobile.mes.controller
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper
 import com.t3rik.common.constant.UserConstants
 import com.t3rik.common.core.controller.BaseController
 import com.t3rik.common.core.page.TableDataInfo
+import com.t3rik.common.utils.SecurityUtils
+import com.t3rik.common.utils.StringUtils
 import com.t3rik.mes.pro.domain.ProTask
+import com.t3rik.mes.pro.dto.TaskDTO
 import com.t3rik.mobile.mes.service.IFeedbackService
+import com.t3rik.mobile.mes.service.IRtIssueService
+import com.t3rik.mobile.mes.service.impl.RtIssueServiceImpl
 import io.swagger.annotations.ApiOperation
 import jakarta.annotation.Resource
 import org.springframework.web.bind.annotation.GetMapping
@@ -18,19 +24,20 @@ import org.springframework.web.bind.annotation.RestController
  * @date 2025/3/9 22:43
  */
 @RestController
-@RequestMapping(UserConstants.MOBILE_PATH + "/rtissue")
+@RequestMapping(UserConstants.MOBILE_PATH + "/rtIssue")
 class RtIssueController : BaseController() {
-
     @Resource
-    lateinit var feedbackService: IFeedbackService;
-
-
+    lateinit var rtIssueService: IRtIssueService
     /**
      * 查询任务列表
      */
     @ApiOperation("查询报工列表")
     @GetMapping("/list")
     fun getTaskList(task: ProTask): TableDataInfo {
-        return getDataTableWithPage(feedbackService.getPageByCurrentIndex(task, getMPPage(task)))
+        val page = getMPPage(TaskDTO())
+        val queryWrapper = QueryWrapper<TaskDTO>()
+        queryWrapper.likeRight(StringUtils.isNotBlank(task.taskName), "task_name", task.taskName)
+        queryWrapper.eq("task_user_id", SecurityUtils.getUserId())
+        return getDataTableWithPage(this.rtIssueService.getTaskListAndIssueCount(page, queryWrapper))
     }
 }
--- t3rik-mobile/src/main/kotlin/com/t3rik/mobile/mes/service/IRtIssueService.kt
+++ t3rik-mobile/src/main/kotlin/com/t3rik/mobile/mes/service/IRtIssueService.kt
@@ -1,6 +1,13 @@
 package com.t3rik.mobile.mes.service
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper
+import com.baomidou.mybatisplus.core.metadata.IPage
+import com.baomidou.mybatisplus.core.toolkit.Constants
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page
+import com.t3rik.mes.pro.domain.ProTask
+import com.t3rik.mes.pro.dto.TaskDTO
 import com.t3rik.mes.wm.domain.WmRtIssue
+import org.apache.ibatis.annotations.Param
 
 
 /**
@@ -14,4 +21,10 @@ interface IRtIssueService {
      * 新增退料
      */
     fun rtIssue(rtIssue: WmRtIssue)
+
+    /**
+     * 查询任务，根据工单分组展示数据，并统计领料次数
+     * @param query 查询条件
+     */
+    fun getTaskListAndIssueCount(page: IPage<TaskDTO>,@Param(Constants.WRAPPER) query: Wrapper<TaskDTO>): Page<TaskDTO>
 }
