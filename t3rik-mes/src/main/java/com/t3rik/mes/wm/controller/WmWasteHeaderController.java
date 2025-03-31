74bf3359498acdd929fd1c09500e986af2092999
t3rik <wangx452@qq.com>
2025-03-31 20:55:48 +0800

    feat: 修复废料管理页面

--- t3rik-mes/src/main/java/com/t3rik/mes/wm/controller/WmWasteHeaderController.java
+++ t3rik-mes/src/main/java/com/t3rik/mes/wm/controller/WmWasteHeaderController.java
@@ -1,37 +1,29 @@
 package com.t3rik.mes.wm.controller;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import jakarta.servlet.http.HttpServletResponse;
-
 import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.lang.Assert;
-import com.t3rik.common.constant.UserConstants;
-import com.t3rik.common.utils.DateUtils;
-import com.t3rik.mes.wm.domain.*;
-import com.t3rik.mes.wm.domain.tx.WmWasteTxBean;
-import com.t3rik.mes.wm.service.*;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.t3rik.common.annotation.Log;
+import com.t3rik.common.constant.UserConstants;
 import com.t3rik.common.core.controller.BaseController;
 import com.t3rik.common.core.domain.AjaxResult;
-import com.t3rik.common.utils.StringUtils;
-import com.t3rik.common.enums.BusinessType;
-import com.t3rik.common.utils.poi.ExcelUtil;
 import com.t3rik.common.core.page.TableDataInfo;
+import com.t3rik.common.enums.BusinessType;
+import com.t3rik.common.utils.DateUtils;
+import com.t3rik.common.utils.StringUtils;
+import com.t3rik.common.utils.poi.ExcelUtil;
+import com.t3rik.mes.wm.domain.*;
+import com.t3rik.mes.wm.domain.tx.WmWasteTxBean;
+import com.t3rik.mes.wm.service.*;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * 生产废料单头Controller
@@ -42,22 +34,22 @@ import com.t3rik.common.core.page.TableDataInfo;
 @RestController
 @RequestMapping("/mes/wm-waste-header")
 public class WmWasteHeaderController extends BaseController {
-    @Autowired
+    @Resource
     private IWmWasteHeaderService wmWasteHeaderService;
 
-    @Autowired
+    @Resource
     private IWmWasteLineService wmWasteLineService;
 
-	@Autowired
+    @Resource
     private IWmWarehouseService wmWarehouseService;
 
-    @Autowired
+    @Resource
     private IWmStorageLocationService wmStorageLocationService;
 
-    @Autowired
+    @Resource
     private IWmStorageAreaService wmStorageAreaService;
 
-    @Autowired
+    @Resource
     private IStorageCoreService storageCoreService;
 
     /**
@@ -85,7 +77,7 @@ public class WmWasteHeaderController extends BaseController {
         // 获取查询条件
         LambdaQueryWrapper<WmWasteHeader> queryWrapper = getQueryWrapper(wmWasteHeader);
         List<WmWasteHeader> list = this.wmWasteHeaderService.list(queryWrapper);
-        ExcelUtil<WmWasteHeader> util = new ExcelUtil<WmWasteHeader>(WmWasteHeader. class);
+        ExcelUtil<WmWasteHeader> util = new ExcelUtil<WmWasteHeader>(WmWasteHeader.class);
         util.exportExcel(response, list, "生产废料单头数据");
     }
 
@@ -106,22 +98,22 @@ public class WmWasteHeaderController extends BaseController {
     @PostMapping
     @Transactional
     public AjaxResult add(@RequestBody WmWasteHeader wmWasteHeader) {
-        //查询退料单号是否已存在
-        if(UserConstants.NOT_UNIQUE.equals(wmWasteHeaderService.checkUnique(wmWasteHeader))){
+        // 查询退料单号是否已存在
+        if (UserConstants.NOT_UNIQUE.equals(wmWasteHeaderService.checkUnique(wmWasteHeader))) {
             return AjaxResult.error("退料单编号已存在");
         }
 
-        if(StringUtils.isNotNull(wmWasteHeader.getWarehouseId())){
+        if (StringUtils.isNotNull(wmWasteHeader.getWarehouseId())) {
             WmWarehouse warehouse = wmWarehouseService.selectWmWarehouseByWarehouseId(wmWasteHeader.getWarehouseId());
             wmWasteHeader.setWarehouseCode(warehouse.getWarehouseCode());
             wmWasteHeader.setWarehouseName(warehouse.getWarehouseName());
         }
-        if(StringUtils.isNotNull(wmWasteHeader.getLocationId())){
+        if (StringUtils.isNotNull(wmWasteHeader.getLocationId())) {
             WmStorageLocation location = wmStorageLocationService.selectWmStorageLocationByLocationId(wmWasteHeader.getLocationId());
             wmWasteHeader.setLocationCode(location.getLocationCode());
             wmWasteHeader.setLocationName(location.getLocationName());
         }
-        if(StringUtils.isNotNull(wmWasteHeader.getAreaId())){
+        if (StringUtils.isNotNull(wmWasteHeader.getAreaId())) {
             WmStorageArea area = wmStorageAreaService.selectWmStorageAreaByAreaId(wmWasteHeader.getAreaId());
             wmWasteHeader.setAreaCode(area.getAreaCode());
             wmWasteHeader.setAreaName(area.getAreaName());
@@ -136,27 +128,27 @@ public class WmWasteHeaderController extends BaseController {
     @Log(title = "生产废料单头", businessType = BusinessType.UPDATE)
     @PutMapping
     public AjaxResult edit(@RequestBody WmWasteHeader wmWasteHeader) {
-        //查询退料单号是否已存在
-        if(UserConstants.NOT_UNIQUE.equals(wmWasteHeaderService.checkUnique(wmWasteHeader))){
+        // 查询退料单号是否已存在
+        if (UserConstants.NOT_UNIQUE.equals(wmWasteHeaderService.checkUnique(wmWasteHeader))) {
             return AjaxResult.error("退料单编号已存在");
         }
 
-        if(StringUtils.isNotNull(wmWasteHeader.getWarehouseId())){
+        if (StringUtils.isNotNull(wmWasteHeader.getWarehouseId())) {
             WmWarehouse warehouse = wmWarehouseService.selectWmWarehouseByWarehouseId(wmWasteHeader.getWarehouseId());
             wmWasteHeader.setWarehouseCode(warehouse.getWarehouseCode());
             wmWasteHeader.setWarehouseName(warehouse.getWarehouseName());
         }
-        if(StringUtils.isNotNull(wmWasteHeader.getLocationId())){
+        if (StringUtils.isNotNull(wmWasteHeader.getLocationId())) {
             WmStorageLocation location = wmStorageLocationService.selectWmStorageLocationByLocationId(wmWasteHeader.getLocationId());
             wmWasteHeader.setLocationCode(location.getLocationCode());
             wmWasteHeader.setLocationName(location.getLocationName());
         }
-        if(StringUtils.isNotNull(wmWasteHeader.getAreaId())){
+        if (StringUtils.isNotNull(wmWasteHeader.getAreaId())) {
             WmStorageArea area = wmStorageAreaService.selectWmStorageAreaByAreaId(wmWasteHeader.getAreaId());
             wmWasteHeader.setAreaCode(area.getAreaCode());
             wmWasteHeader.setAreaName(area.getAreaName());
         }
-        //修改人、时间
+        // 修改人、时间
 //        wmWasteHeader.setUpdateBy(getUsername());
 //        wmWasteHeader.setUpdateById(getUserId());
         wmWasteHeader.setUpdateTime(DateUtils.getNowDate());
@@ -170,35 +162,36 @@ public class WmWasteHeaderController extends BaseController {
     @Log(title = "生产废料单头", businessType = BusinessType.DELETE)
     @DeleteMapping("/{wasteIds}")
     public AjaxResult remove(@PathVariable List<Long> wasteIds) {
-        //删除行信息
+        // 删除行信息
         wmWasteLineService.delWmWasteLineIds(wasteIds);
-        //删除头数据
+        // 删除头数据
         return toAjax(wmWasteHeaderService.delWmWasteHeaderIds(wasteIds));
     }
 
     /**
      * 执行废料
+     *
      * @param wasteId
      * @return
      */
     @PreAuthorize("@ss.hasPermi('mes:wmwasteheader:edit')")
     @Log(title = "生产废料单头", businessType = BusinessType.UPDATE)
     @PutMapping("/{wasteId}")
-    public AjaxResult execute(@PathVariable Long wasteId){
-        //查询生产退料单头根据id
+    public AjaxResult execute(@PathVariable Long wasteId) {
+        // 查询生产退料单头根据id
         WmWasteHeader wasteHeader = wmWasteHeaderService.lambdaQuery().eq(WmWasteHeader::getWasteId, wasteId).one();
-        //查询生产退料行信息
+        // 查询生产退料行信息
         List<WmWasteLine> wmWasteLines = wmWasteLineService.selectWmWasteLineList(wasteHeader.getWasteId());
-        //判断数据石头为空
+        // 判断数据石头为空
         if (CollUtil.isEmpty(wmWasteLines)) {
             return AjaxResult.error("请选择要废料的物资");
         }
-        //查询废料信息所对应的库存记录
+        // 查询废料信息所对应的库存记录
         List<WmWasteTxBean> beans = wmWasteHeaderService.getTxBeans(wasteId);
-        //执行废料
+        // 执行废料
         storageCoreService.processWmWaste(beans);
 
-        //修改状态已完成
+        // 修改状态已完成
         wasteHeader.setStatus(UserConstants.ORDER_STATUS_FINISHED);
         wmWasteHeaderService.updateById(wasteHeader);
 
@@ -206,12 +199,12 @@ public class WmWasteHeaderController extends BaseController {
     }
 
     /**
-    * 获取查询条件
-    */
+     * 获取查询条件
+     */
     public LambdaQueryWrapper<WmWasteHeader> getQueryWrapper(WmWasteHeader wmWasteHeader) {
         LambdaQueryWrapper<WmWasteHeader> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(wmWasteHeader.getRecordId() != null, WmWasteHeader::getRecordId, wmWasteHeader.getRecordId());
-        queryWrapper.eq(wmWasteHeader.getWasteCode() != null, WmWasteHeader::getWasteCode, wmWasteHeader.getWasteCode());
+        queryWrapper.eq(StringUtils.isNotEmpty(wmWasteHeader.getWasteCode()), WmWasteHeader::getWasteCode, wmWasteHeader.getWasteCode());
         queryWrapper.like(StringUtils.isNotEmpty(wmWasteHeader.getWasteName()), WmWasteHeader::getWasteName, wmWasteHeader.getWasteName());
         queryWrapper.eq(wmWasteHeader.getWorkorderId() != null, WmWasteHeader::getWorkorderId, wmWasteHeader.getWorkorderId());
         queryWrapper.eq(wmWasteHeader.getWorkorderCode() != null, WmWasteHeader::getWorkorderCode, wmWasteHeader.getWorkorderCode());
@@ -225,7 +218,7 @@ public class WmWasteHeaderController extends BaseController {
         queryWrapper.eq(wmWasteHeader.getAreaCode() != null, WmWasteHeader::getAreaCode, wmWasteHeader.getAreaCode());
         queryWrapper.like(StringUtils.isNotEmpty(wmWasteHeader.getAreaName()), WmWasteHeader::getAreaName, wmWasteHeader.getAreaName());
         queryWrapper.eq(wmWasteHeader.getWasteDate() != null, WmWasteHeader::getWasteDate, wmWasteHeader.getWasteDate());
-        Optional.ofNullable(wmWasteHeader.getStatus()).ifPresent(status -> queryWrapper.eq( WmWasteHeader::getStatus, wmWasteHeader.getStatus()));
+        Optional.ofNullable(wmWasteHeader.getStatus()).ifPresent(status -> queryWrapper.eq(WmWasteHeader::getStatus, wmWasteHeader.getStatus()));
         queryWrapper.eq(wmWasteHeader.getAttr1() != null, WmWasteHeader::getAttr1, wmWasteHeader.getAttr1());
         queryWrapper.eq(wmWasteHeader.getAttr2() != null, WmWasteHeader::getAttr2, wmWasteHeader.getAttr2());
         queryWrapper.eq(wmWasteHeader.getAttr3() != null, WmWasteHeader::getAttr3, wmWasteHeader.getAttr3());
@@ -233,7 +226,7 @@ public class WmWasteHeaderController extends BaseController {
         // 默认创建时间倒序
         queryWrapper.orderByDesc(WmWasteHeader::getCreateTime);
         Map<String, Object> params = wmWasteHeader.getParams();
-        queryWrapper.between(params.get("beginTime") != null && params.get("endTime") != null,WmWasteHeader::getCreateTime, params.get("beginTime"), params.get("endTime"));
+        queryWrapper.between(params.get("beginTime") != null && params.get("endTime") != null, WmWasteHeader::getCreateTime, params.get("beginTime"), params.get("endTime"));
         return queryWrapper;
     }
 }
