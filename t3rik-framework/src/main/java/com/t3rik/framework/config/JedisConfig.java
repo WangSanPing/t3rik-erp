61542ba1c5a8d4a653384e5e98d12d439e32df00
t3rik <wangx452@qq.com>
2025-04-04 20:28:21 +0800

    feat: 临时加一个黑名单限制防止傻逼

---
+++ t3rik-framework/src/main/java/com/t3rik/framework/config/JedisConfig.java
@@ -0,0 +1,44 @@
+package com.t3rik.framework.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * jedis客户端
+ *
+ * @author t3rik
+ * @date 2025/4/4 20:08
+ */
+@Configuration
+public class JedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    // 配置 Jedis 连接池
+    @Bean()
+    public JedisPool jedisPool() {
+        // 设置连接池配置
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(10); // 最大连接数
+        poolConfig.setMaxIdle(5);   // 最大空闲连接数
+        poolConfig.setMinIdle(1);   // 最小空闲连接数
+        poolConfig.setTestOnBorrow(true);  // 在借用连接时进行测试
+        poolConfig.setJmxEnabled(false); // 禁用 JMX 注册
+        // 创建 Jedis 连接池，连接 Redis 服务
+        return new JedisPool(poolConfig, host, 6379, 5000, password);
+    }
+
+    // 配置 Jedis 实例
+    @Bean
+    public Jedis jedis(JedisPool jedisPool) {
+        return jedisPool.getResource();
+    }
+}
