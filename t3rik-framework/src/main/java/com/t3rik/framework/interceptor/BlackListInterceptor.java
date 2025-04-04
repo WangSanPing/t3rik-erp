84aa077c36cf4cc67a659cc7a07b2b4089c260f8
t3rik <wangx452@qq.com>
2025-04-04 19:18:09 +0800

    feat: 临时加一个黑名单限制防止傻逼

---
+++ t3rik-framework/src/main/java/com/t3rik/framework/interceptor/BlackListInterceptor.java
@@ -0,0 +1,59 @@
+package com.t3rik.framework.interceptor;
+
+import com.t3rik.common.exception.BusinessException;
+import com.t3rik.common.utils.ip.IpUtils;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 黑名单拦截器
+ *
+ * @author t3rik
+ * @date 2025/4/4 19:03
+ */
+@Slf4j
+@Component
+public class BlackListInterceptor implements HandlerInterceptor {
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String ipAddr = IpUtils.getIpAddr(request);
+        String BLACK_LIST_KEY = "ip:blacklist";
+        String ipKey = "ip:count:" + ipAddr;
+
+        // 先检查是否在黑名单中
+        if (Boolean.TRUE.equals(redisTemplate.opsForSet().isMember(BLACK_LIST_KEY, ipAddr))) {
+            log.warn("当前ip已被限制黑名单访问 : {}", ipAddr);
+            throw new BusinessException("短时间内访问次数过多，已被限制访问");
+        }
+
+        // 自增访问次数，如果是第一次访问，设置过期时间为1分钟
+        Long count = redisTemplate.opsForValue().increment(ipKey);
+        if (count != null && count == 1) {
+            // 设置访问计数键 1 分钟后过期
+            redisTemplate.expire(ipKey, 1, TimeUnit.MINUTES);
+        }
+
+        // 超过15次访问，则加入黑名单
+        if (count != null && count > 15) {
+            redisTemplate.opsForSet().add(BLACK_LIST_KEY, ipAddr);
+        }
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+}
