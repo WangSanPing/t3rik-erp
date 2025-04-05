ee272abc5e369fdf515f66a9b4a7be6b5d77afa7
t3rik <wangx452@qq.com>
2025-04-05 20:31:45 +0800

    feat: 规范依赖

--- t3rik-common/src/main/java/com/t3rik/common/utils/file/FileUploadUtils.java
+++ t3rik-common/src/main/java/com/t3rik/common/utils/file/FileUploadUtils.java
@@ -108,69 +108,8 @@ public class FileUploadUtils {
     }
 
    
-    /**
-     * 自定义bucketName配置上传到Minio服务器
-     *
-     * @param file 上传的文件
-     * @return 文件名称
-     * @throws Exception
-     */
-    public static final String uploadMinio(MultipartFile file, String bucketName) throws IOException {
-        try {
-            return uploadMinino(bucketName, file, MimeTypeUtils.DEFAULT_ALLOWED_EXTENSION);
-        } catch (Exception e) {
-            throw new IOException(e.getMessage(), e);
-        }
-    }
 
-    /**
-     * 自定义bucketName配置上传到Minio服务器
-     *
-     * @param file 上传的文件
-     * @return 文件名称
-     * @throws Exception
-     */
-    public static final String uploadMinio(File file, String bucketName) throws IOException {
-        try {
-            return uploadMinino(bucketName, file, MimeTypeUtils.DEFAULT_ALLOWED_EXTENSION);
-        } catch (Exception e) {
-            throw new IOException(e.getMessage(), e);
-        }
-    }
 
-    private static final String uploadMinino(String bucketName, MultipartFile file, String[] allowedExtension)
-            throws FileSizeLimitExceededException, IOException, FileNameLengthLimitExceededException,
-            InvalidExtensionException {
-        int fileNamelength = file.getOriginalFilename().length();
-        if (fileNamelength > FileUploadUtils.DEFAULT_FILE_NAME_LENGTH) {
-            throw new FileNameLengthLimitExceededException(FileUploadUtils.DEFAULT_FILE_NAME_LENGTH);
-        }
-        assertAllowed(file, allowedExtension);
-        try {
-            String fileName = extractFilename(file);
-            String pathFileName = MinioUtil.uploadFile(bucketName, fileName, file);
-            return pathFileName;
-        } catch (Exception e) {
-            throw new IOException(e.getMessage(), e);
-        }
-    }
-
-    private static final String uploadMinino(String bucketName, File file, String[] allowedExtension)
-            throws FileSizeLimitExceededException, IOException, FileNameLengthLimitExceededException,
-            InvalidExtensionException {
-        int fileNamelength = file.getName().length();
-        if (fileNamelength > FileUploadUtils.DEFAULT_FILE_NAME_LENGTH) {
-            throw new FileNameLengthLimitExceededException(FileUploadUtils.DEFAULT_FILE_NAME_LENGTH);
-        }
-        assertAllowed(file, allowedExtension);
-        try {
-            String fileName = extractFilename(file);
-            String pathFileName = MinioUtil.uploadFile(bucketName, fileName, file);
-            return pathFileName;
-        } catch (Exception e) {
-            throw new IOException(e.getMessage(), e);
-        }
-    }
 
     /**
      * 编码文件名
