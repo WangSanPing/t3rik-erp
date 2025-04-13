833d40c40677fdabf13a3c9a0432c3e8d3d986e3
t3rik <wangx452@qq.com>
2025-04-13 17:14:42 +0800

    feat: 删除推送功能

--- t3rik-mes/src/main/java/com/t3rik/mes/api/mapper/AccessMapper.java
+++
@@ -1,9 +0,0 @@
-package com.t3rik.mes.api.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.t3rik.mes.api.domain.Access;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AccessMapper extends BaseMapper<Access>{
-}
